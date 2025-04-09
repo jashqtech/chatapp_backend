b5a4f8bf8cf08bee855e042876a294cc9c50d2d4
jashqtech <jash.a@qrioustech.in>
2025-04-09 12:13:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,53 +4,62 @@ const socketIO = require("socket.io");
 const cors = require("cors");
 
 const app = express();
+app.use(cors());
+
 const server = http.createServer(app);
-const io = require("socket.io")(process.env.PORT || 4000, {
+const io = socketIO(server, {
   cors: { origin: "*" }
 });
 
-  
-  const userSocketMap = {};
-  
-  const getOnlineUsers = () => {
-    return Object.keys(userSocketMap).reduce((acc, username) => {
-      acc[username] = true;
-      return acc;
-    }, {});
-  };
-  
-  io.on("connection", (socket) => {
-    console.log("A user connected:", socket.id);
-  
-    socket.on("register", (username) => {
-      userSocketMap[username] = socket.id;
-      socket.username = username;
-      console.log(`${username} registered with socket ${socket.id}`);
-      io.emit("user_online_status", getOnlineUsers()); // broadcast status
-    });
-  
-    socket.on("private_message", (data) => {
-      const { sender, receiver, message } = data;
-      const receiverSocket = userSocketMap[receiver];
-      if (receiverSocket) {
-        io.to(receiverSocket).emit("private_message", data);
-      } else {
-        io.to(socket.id).emit("error_message", { error: "User is offline." });
-      }
-    });
-  
-    socket.on("typing", ({ sender, receiver }) => {
-      const receiverSocket = userSocketMap[receiver];
-      if (receiverSocket) {
-        io.to(receiverSocket).emit("typing", { sender, receiver });
-      }
-    });
-  
-    socket.on("disconnect", () => {
-      if (socket.username) {
-        delete userSocketMap[socket.username];
-        io.emit("user_online_status", getOnlineUsers());
-      }
-      console.log("User disconnected:", socket.id);
-    });
+const PORT = process.env.PORT || 4000;
+
+const userSocketMap = {};
+
+app.get("/", (req, res) => {
+  res.send("Socket.IO Server is running");
+});
+
+const getOnlineUsers = () => {
+  return Object.keys(userSocketMap).reduce((acc, username) => {
+    acc[username] = true;
+    return acc;
+  }, {});
+};
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("register", (username) => {
+    userSocketMap[username] = socket.id;
+    socket.username = username;
+    io.emit("user_online_status", getOnlineUsers());
   });
+
+  socket.on("private_message", ({ sender, receiver, message }) => {
+    const receiverSocket = userSocketMap[receiver];
+    if (receiverSocket) {
+      io.to(receiverSocket).emit("private_message", { sender, receiver, message });
+    } else {
+      io.to(socket.id).emit("error_message", { error: "User is offline." });
+    }
+  });
+
+  socket.on("typing", ({ sender, receiver }) => {
+    const receiverSocket = userSocketMap[receiver];
+    if (receiverSocket) {
+      io.to(receiverSocket).emit("typing", { sender, receiver });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    if (socket.username) {
+      delete userSocketMap[socket.username];
+      io.emit("user_online_status", getOnlineUsers());
+    }
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
